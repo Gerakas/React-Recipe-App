bce2d98ab840d8eb8059f06f649786868ecc9ceb
Gerakas <andreasgerakianakis@gmail.com>
2021-04-15 23:25:08 +0100

    Created 'getRecipes' function to test and fetch the example GET request from the API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 const App = () => {
 
   const APP_ID = '28ad872e';
   const APP_KEY = 'c0cbb11915531768a3bd9de43716f761';
-  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data);
+  }
 
 
   return (
