6df2a2f96e1ab2259bf82e778591058b9753a5d3
Gerakas <andreasgerakianakis@gmail.com>
2021-04-15 21:53:50 +0100

    Added the recipe search  API information to pull information

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <h1>Hello!</h1>
-    )
-  }
+const App = () => {
+
+  const APP_ID = '28ad872e';
+  const APP_KEY = 'c0cbb11915531768a3bd9de43716f761';
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+
+  return (
+    <div className="App">
+      <form className="search-form"> 
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">Search</button>
+      </form>
+    </div>
+  )
 }
 
 export default App;
